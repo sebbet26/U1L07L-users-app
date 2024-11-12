8689681af6c03fa740f22befcc3d95c8c8177bad
sebbet26 <sebbet26@bergen.org>
2024-11-12 14:08:26 -0500

    New Post Page

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def register(request):
-     context = {
-        'active_link': 'register'
+    # Check if the form is submitted
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) # Submitted with data
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm
+    context = {
+        'active_link': 'register',
+        'form': form
     }
-     return render(request, 'users/register.html', context)
+    return render(request, 'users/register.html', context)
